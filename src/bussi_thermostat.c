1f72ef3a088d25a0f08ae396bb0abfb52fe644e3
Andrew White <white.d.andrew@gmail.com>
2013-04-22 21:56:54 -0700

    After some math thinking, I've modified the implementation of the Bussi thermostat. Also got it to compile

--- src/bussi_thermostat.c
+++ src/bussi_thermostat.c
@@ -2,6 +2,49 @@
 #include <gsl/gsl_randist.h>
 #include <math.h>
 
+
+
+/*
+ * Sampling exactly when less than 6, instead of exactly 1. TODO: Check if this
+ * is necessary.
+ */
+double resamplekin_sumnoises(gsl_rng* rng, unsigned int nn){
+/*
+  returns the sum of n independent gaussian noises squared
+   (i.e. equivalent to summing the square of the return values of nn calls to gasdev)
+*/
+  double rr;
+  if(nn == 0) {
+    return 0.0;
+  } else if(nn % 2 == 0) {
+    return 2.0 * gsl_ran_gamma(rng, nn/2, 1.0);
+  } else {
+    rr= gsl_ran_gaussian_ziggurat(rng, 1);
+    return 2.0 * gsl_ran_gamma(rng, (nn-1)/2, 1.0) + rr*rr;
+  }
+}
+
+
+/* Equation A7 from Bussi2007
+  kk:    present value of the kinetic energy of the atoms to be thermalized (in arbitrary units)
+  sigma: target average value of the kinetic energy (ndeg k_b T/2)  (in the same units as kk)
+  ndeg:  number of degrees of freedom of the atoms to be thermalized
+  taut:  relaxation time of the thermostat, in units of 'how often this routine is called'
+*/
+
+double resamplekin(gsl_rng* rng, double kk, double sigma, int ndeg, double taut){
+  double factor,rr;
+  if(taut>0.1){
+    factor=exp(-1.0/taut);
+  } else{
+    factor=0.0;
+  }
+  rr = gsl_ran_gaussian_ziggurat(rng, 1);
+  return kk + (1.0-factor)* (sigma*(resamplekin_sumnoises(rng, ndeg-1)+rr*rr)/ndeg-kk)
+            + 2.0*rr*sqrt(kk*sigma/ndeg*(1.0-factor)*factor);
+}
+
+
 void thermostat(double temperature, double time_step, void* thermostat_parameters, double* positions, double* velocities, double* masses, unsigned int n_dims, unsigned int n_particles) {
 
   Bussi_Params* params = (Bussi_Params*) thermostat_parameters;
@@ -22,7 +65,7 @@ void thermostat(double temperature, double time_step, void* thermostat_parameter
   ndeg = (n_particles * n_dims - n_dims);
 
   //Equation A7 from Bussi2007
-  scaling_factor = resamplekin(kenergy, 0.5 * temperature * ndeg, ndeg, params->taut);
+  scaling_factor = resamplekin(params->rng, kenergy, 0.5 * temperature * ndeg, ndeg, params->taut);
   scaling_factor = sqrt(scaling_factor);
 
   for(i = 0; i < n_particles; i++) {
@@ -33,49 +76,6 @@ void thermostat(double temperature, double time_step, void* thermostat_parameter
 
 }
 
-
-/* Equation A7 from Bussi2007
-  kk:    present value of the kinetic energy of the atoms to be thermalized (in arbitrary units)
-  sigma: target average value of the kinetic energy (ndeg k_b T/2)  (in the same units as kk)
-  ndeg:  number of degrees of freedom of the atoms to be thermalized
-  taut:  relaxation time of the thermostat, in units of 'how often this routine is called'
-*/
-
-double resamplekin(double kk,double sigma, int ndeg, double taut){
-  double factor,rr;
-  if(taut>0.1){
-    factor=exp(-1.0/taut);
-  } else{
-    factor=0.0;
-  }
-  rr = gasdev();
-  return kk + (1.0-factor)* (sigma*(resamplekin_sumnoises(ndeg-1)+rr*rr)/ndeg-kk)
-            + 2.0*rr*sqrt(kk*sigma/ndeg*(1.0-factor)*factor);
-}
-
-
-/*
- * Sampling exactly when less than 6, instead of exactly 1. TODO: Check if this
- * is necessary.
- */
-double resamplekin_sumnoises(gsl_rng* rng, unsigned int nn){
-/*
-  returns the sum of n independent gaussian noises squared
-   (i.e. equivalent to summing the square of the return values of nn calls to gasdev)
-*/
-  double rr;
-  if(nn == 0) {
-    return 0.0;
-  } else if(nn < 6) {
-    rr = gsl_ran_gaussian_ziggurat(rng,1);
-    return rr * rr;
-  } else if(nn % 2 == 0) {
-    return 2.0 * gsl_ran_gamma(rng, nn/2, 1.0);
-  } else {
-    rr=gsl_ran_gaussian_ziggurat(rng, 1);
-    return 2.0 * gsl_ran_gamma(rng, (nn-1)/2, 1.0) + rr*rr;
-  }
-}
 
 
 void* build_bussi(unsigned int seed, double taut) {
--- src/thermostat.h
+++ src/thermostat.h
@@ -3,25 +3,31 @@
 #ifndef THERMOSTAT_H_
 #define THERMOSTAT_H_
 
-#ifdef ANDERSON
-typedef struct {
-  gsl_rng * rng;
-  double nu;
-} Anderson_Params;
-#endif
-
-#ifdef BUSSI
-typedef struct {
-  gsl_rng * rng;
-  double taut;
-} Bussi_Params;
-#endif
 
 void thermostat(double temperature, double time_step, void* thermostat_parameters, double* positions, double* velocities, double* masses, unsigned int n_dims, unsigned int n_particles);
 
 #ifdef ANDERSON
 #define THERMOSTAT
+
+typedef struct {
+  gsl_rng * rng;
+  double nu;
+} Anderson_Params;
+
 void* build_anderson(unsigned int seed, double collision_freq);
-#endif
 
 #endif
+
+#ifdef BUSSI
+#define THERMOSTAT
+
+typedef struct {
+  gsl_rng * rng;
+  double taut;
+} Bussi_Params;
+
+void* build_bussi(unsigned int seed, double taut);
+
+#endif
+
+#endif //THERMOSTAT_H_
